524bbef210019d4f081002f45d9dd7e6680fc594
zhukandrey <zhuk.andrey.a@gmail.com>
2023-11-14 21:20:45 +0100

    Improve page layout

--- packages/nextjs/pages/owners.tsx
+++ packages/nextjs/pages/owners.tsx
@@ -18,30 +18,36 @@ const Owners: NextPage = () => {
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
+        <div className="p-2">
           <ul>
             {events?.map(event => (
               <li key={event.log.logIndex}>
-                <Address address={event.args.owner} /> - {event.args.added ? "TRUE" : "FALSE"}
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <Address address={event.args.owner} />
+                  <span>{event.args.added ? "TRUE" : "FALSE"}</span>
+                </div>
               </li>
             ))}
           </ul>
+          <div className={"p-2"}>
+            <label className="text-lg font-bold">Owner address</label>
+            <AddressInput
+              name={"ownerAddress"}
+              onChange={v => setOwnerAddressValue(v)}
+              value={ownerAddressValue}
+              placeholder={"Owner address"}
+            />
+          </div>
 
-          <label className="text-lg font-bold">Owner address</label>
-          <AddressInput
-            name={"ownerAddress"}
-            onChange={v => setOwnerAddressValue(v)}
-            value={ownerAddressValue}
-            placeholder={"Owner address"}
-          />
-
-          <label className="text-lg font-bold">New number of required confirmations</label>
-          <IntegerInput
-            name={"newNumberOfRequiredConfirmations"}
-            onChange={v => setNewNumberOfRequiredConfirmationsValue(v.toString())}
-            value={newNumberOfRequiredConfirmationsValue}
-            placeholder={"New number of required confirmations"}
-          />
+          <div className={"p-2"}>
+            <label className="text-lg font-bold">New number of required confirmations</label>
+            <IntegerInput
+              name={"newNumberOfRequiredConfirmations"}
+              onChange={v => setNewNumberOfRequiredConfirmationsValue(v.toString())}
+              value={newNumberOfRequiredConfirmationsValue}
+              placeholder={"New number of required confirmations"}
+            />
+          </div>
         </div>
       </div>
     </>
