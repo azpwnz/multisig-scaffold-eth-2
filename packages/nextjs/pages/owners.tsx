c832f7856ac213363935390a0913f71c592543a3
zhukandrey <zhuk.andrey.a@gmail.com>
2023-11-14 21:07:44 +0100

    Add owners page

---
+++ packages/nextjs/pages/owners.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+import { MetaHeader } from "~~/components/MetaHeader";
+import { AddressInput, IntegerInput } from "~~/components/scaffold-eth";
+import { Address } from "~~/components/scaffold-eth/Address";
+import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
+
+const Owners: NextPage = () => {
+  const [ownerAddressValue, setOwnerAddressValue] = useState("");
+  const [newNumberOfRequiredConfirmationsValue, setNewNumberOfRequiredConfirmationsValue] = useState("");
+
+  const { data: events } = useScaffoldEventHistory({
+    contractName: "MetaMultiSigWallet",
+    eventName: "Owner",
+    fromBlock: 0n,
+  });
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <ul>
+            {events?.map(event => (
+              <li key={event.log.logIndex}>
+                <Address address={event.args.owner} /> - {event.args.added ? "TRUE" : "FALSE"}
+              </li>
+            ))}
+          </ul>
+
+          <label className="text-lg font-bold">Owner address</label>
+          <AddressInput
+            name={"ownerAddress"}
+            onChange={v => setOwnerAddressValue(v)}
+            value={ownerAddressValue}
+            placeholder={"Owner address"}
+          />
+
+          <label className="text-lg font-bold">New number of required confirmations</label>
+          <IntegerInput
+            name={"newNumberOfRequiredConfirmations"}
+            onChange={v => setNewNumberOfRequiredConfirmationsValue(v.toString())}
+            value={newNumberOfRequiredConfirmationsValue}
+            placeholder={"New number of required confirmations"}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Owners;
