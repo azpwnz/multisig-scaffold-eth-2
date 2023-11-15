b1f7e003d4e3a0ace248730006780c9ceabb889b
zhukandrey <zhuk.andrey.a@gmail.com>
2023-11-15 08:53:28 +0100

    Implement buttons to add/remove signers

--- packages/nextjs/pages/owners.tsx
+++ packages/nextjs/pages/owners.tsx
@@ -3,16 +3,28 @@ import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { AddressInput, IntegerInput } from "~~/components/scaffold-eth";
 import { Address } from "~~/components/scaffold-eth/Address";
-import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
+import { useScaffoldContractWrite, useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const Owners: NextPage = () => {
-  const [ownerAddressValue, setOwnerAddressValue] = useState("");
-  const [newNumberOfRequiredConfirmationsValue, setNewNumberOfRequiredConfirmationsValue] = useState("");
+  const [signerAddressValue, setSignerAddressValue] = useState("");
+  const [newNumberOfSignaturesValue, setNewNumberOfSignaturesValue] = useState("");
 
   const { data: events } = useScaffoldEventHistory({
     contractName: "MetaMultiSigWallet",
     eventName: "Owner",
     fromBlock: 0n,
+  });
+
+  const { writeAsync: addSigner } = useScaffoldContractWrite({
+    contractName: "MetaMultiSigWallet",
+    functionName: "addSigner",
+    args: [signerAddressValue, BigInt(newNumberOfSignaturesValue)],
+  });
+
+  const { writeAsync: removeSigner } = useScaffoldContractWrite({
+    contractName: "MetaMultiSigWallet",
+    functionName: "removeSigner",
+    args: [signerAddressValue, BigInt(newNumberOfSignaturesValue)],
   });
   return (
     <>
@@ -33,20 +45,31 @@ const Owners: NextPage = () => {
             <label className="text-lg font-bold">Owner address</label>
             <AddressInput
               name={"ownerAddress"}
-              onChange={v => setOwnerAddressValue(v)}
-              value={ownerAddressValue}
+              onChange={v => setSignerAddressValue(v)}
+              value={signerAddressValue}
               placeholder={"Owner address"}
             />
           </div>
 
           <div className={"p-2"}>
-            <label className="text-lg font-bold">New number of required confirmations</label>
+            <label className="text-lg font-bold">New number of signatures</label>
             <IntegerInput
-              name={"newNumberOfRequiredConfirmations"}
-              onChange={v => setNewNumberOfRequiredConfirmationsValue(v.toString())}
-              value={newNumberOfRequiredConfirmationsValue}
-              placeholder={"New number of required confirmations"}
+              name={"newNumberOfSignatures"}
+              onChange={v => setNewNumberOfSignaturesValue(v.toString())}
+              value={newNumberOfSignaturesValue}
+              placeholder={"New number of signatures"}
             />
+          </div>
+
+          <div className={"p-2"}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <button className={"btn btn-primary"} onClick={() => addSigner()}>
+                Add signer
+              </button>
+              <button className={"btn btn-primary"} onClick={() => removeSigner()}>
+                Remove signer
+              </button>
+            </div>
           </div>
         </div>
       </div>
