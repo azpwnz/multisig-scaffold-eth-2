bb23541ef7cedc622bbae13a4aab303b424543da
zhukandrey <zhuk.andrey.a@gmail.com>
2023-12-07 21:01:54 +0100

    [WIP] Implementing create page

--- packages/nextjs/pages/create.tsx
+++ packages/nextjs/pages/create.tsx
@@ -1,41 +1,103 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import type { NextPage } from "next";
+import { platform } from "os";
+import { hashMessage } from "viem";
+import { useAccount, useSignMessage } from "wagmi";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { AddressInput, IntegerInput } from "~~/components/scaffold-eth";
 import { EtherInput } from "~~/components/scaffold-eth";
 import { Address } from "~~/components/scaffold-eth/Address";
-import { useScaffoldContractWrite, useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
-import { platform } from "os";
+import {
+  useScaffoldContract,
+  useScaffoldContractRead,
+  useScaffoldContractWrite,
+  useScaffoldEventHistory,
+} from "~~/hooks/scaffold-eth";
 
 const CreateTx: NextPage = () => {
+  const [transactionType, setTransactionType] = useState("Send ETH");
   const [toAddressValue, setToAddressValue] = useState("");
   const [ethAmount, setEthAmount] = useState("");
+  const [messageSignature, setMessageSignature] = useState("");
+  const [isOwner, setIsOwner] = useState();
+  const [nonce, setNonce] = useState(0);
+  const [newHash, setNewHash] = useState("");
+
+  const callData = "0x";
+
+  const { data: metaMultiSigWalletContract } = useScaffoldContract({
+    contractName: "MetaMultiSigWallet",
+  });
+
+  const { data, signMessage } = useSignMessage({
+    onSuccess(data) {
+      console.log("Success", data);
+      setMessageSignature(data);
+    },
+    onError(error) {
+      console.error("Error", error);
+    },
+  });
+
+  const triggerSignMessage = async () => {
+    const nonceResult = await metaMultiSigWalletContract?.read.nonce();
+    setNonce(nonceResult);
+
+    // TODO check if toAddressValue is valid address
+    const newHashResult = await metaMultiSigWalletContract?.read.getTransactionHash([
+      nonce,
+      toAddressValue,
+      BigInt(ethAmount),
+      callData,
+    ]);
+
+    setNewHash(newHashResult);
+
+    if (typeof newHash === "undefined") {
+      console.error("newHash is undefined");
+      return;
+    }
+
+    signMessage({ message: newHash });
+  };
 
   const createTransaction = async () => {
-    let callData = "0x";
-    let executeToAddress = toAddressValue;
-    const newHash = "1";
-    const signature = "1";
-    const recover = "1";
+    const recover = await metaMultiSigWalletContract?.read.recover([newHash, messageSignature]);
+    const isOwnerResult = await metaMultiSigWalletContract?.read.isOwner([recover]);
+    setIsOwner(isOwnerResult);
 
     console.log("--------------------");
     console.log("🍉 createTransaction called");
     console.log("🌳 callData", callData);
-    console.log("🌳 executeToAddress", executeToAddress);
+    console.log("🌳 toAddressValue", toAddressValue);
     console.log("🌳 newHash", newHash);
-    console.log("🌳 signature", signature);
+    console.log("🌳 messageSignature", messageSignature);
     console.log("🌳 recover", recover);
     console.log("🌳 amount", ethAmount);
+    console.log("🌳 nonce", nonce);
+    console.log("🌳 isOwner", isOwner);
     console.log("--------------------");
+
+    // TODO post transaction to backend
   };
+
+  useEffect(() => {
+    if (messageSignature) {
+      createTransaction();
+    }
+  }, [messageSignature]);
 
   return (
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="p-2">
-          <select className="select select-bordered w-full max-w-xs">
-            <option selected>Send ETH</option>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            value={transactionType}
+            onChange={e => setTransactionType(e.target.value)}
+          >
+            <option value="Send ETH">Send ETH</option>
           </select>
         </div>
 
@@ -53,7 +115,9 @@ const CreateTx: NextPage = () => {
         </div>
 
         <div className="p-2">
-          <button className="btn btn-primary" onClick={createTransaction}>Create Tx</button>
+          <button className="btn btn-primary" onClick={triggerSignMessage}>
+            Create Tx
+          </button>
         </div>
       </div>
     </>
