faea21ab67afa0afa22e4cb86d07360ba4b088f5
azpwnz <zhuk.andrey.a@gmail.com>
2024-01-23 15:56:36 +0100

    Implement backend

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to allow cross-origin requests
+app.use(cors());
+
+let transactions = {};
 
 app.listen(3005, () => {
-  console.log('Example app listening on port 3005!');
+  console.log('App listening on port 3005!');
+});
+
+app.post("/", function (request, response) {
+  console.log("Received POST request", request.body); // your JSON
+
+  response.send(request.body); // echo the result back
+
+  const key = request.body.address + "_" + request.body.chainId;
+
+  console.log("key:", key);
+
+  if (!transactions[key]) {
+    transactions[key] = {};
+  }
+
+  transactions[key][request.body.hash] = request.body;
+
+  console.log("transactions", transactions);
 });
